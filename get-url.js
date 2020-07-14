74ae0ed7f5b32d1c66eb7373d086f36b13dbc6d5
Ureeces <Ulysses.Reyes@codeimmersives.com>
2020-07-14 14:28:43 -0400

    Accounted for null argument. Finished getUrl

--- get-url.js
+++ get-url.js
@@ -6,7 +6,14 @@
 // URL is at index 1 of the row
 
 // 1. Simply return the index 1 at the given row, which is a subarray
-
+// 2. If row is equal to null, return null
+const getUrl = function(row) {
+  if(row === null) {
+    return null;
+  }
+  
+  return row[1];
+}
 
 
 // OUR code here.
